566a479bb509f6b4d32b5ba35e98205fb70309e2
Jorge Santamaria <jorsebsant@gmail.com>
2019-10-16 18:00:28 -0500

    fix: remove enter key as part of the code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const pressed = []
-// up,up,down,down,left,right,left,right,b,a,enter
-const code = [38,38,40,40,37,39,37,39,66,65,13]
+// up,up,down,down,left,right,left,right,b,a
+const code = [38,38,40,40,37,39,37,39,66,65]
 
 function KonamiCode(callback){
 
